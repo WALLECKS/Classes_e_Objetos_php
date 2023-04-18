9c3c5973cb96baf246bddcd89838e268f25832f9
wallecks <wallecksalencar9@gmail.com>
2023-04-17 22:47:39 -0300

    estudos

---
+++ metodo_autoload.php
@@ -0,0 +1,37 @@
+<?php
+class Conta
+{
+    // atributos da conta
+    private $Codigo;
+    private $Saldo;
+    public $Nome;
+// manipulando o metodo Codigo da classe
+
+    function setCodigo($Codigo)
+    {
+        $this->Codigo = $Codigo;
+    }
+    function getCodigo()
+    {
+        $this->Codigo;
+    }
+// manipulando o metodo da classe Conta
+
+    function setSaldo($Saldo)
+    {
+        $this->Saldo = $Saldo;
+    }
+    function getSaldo()
+        {return $this->Saldo;}
+
+// manipulando o metodo Nome
+
+    function Nome($nome)
+    {
+        $this->Nome = $nome;
+    }
+
+}
+
+
+?>
---
+++ objeto_conta_autoload.php
@@ -0,0 +1,17 @@
+<?php
+    
+include_once "metodo_autoload.php";
+
+
+// instanciando um novo objeto
+$carlos = new Conta;
+
+// atribuindo valores no atributos
+$carlos->Nome = "Carlos Eduardo";
+$carlos->setSaldo(1500);
+
+// mostrando os valores dos atributos
+
+var_dump($carlos->Nome);
+var_dump($carlos->getSaldo());
+?>
