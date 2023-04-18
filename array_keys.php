89bae216d45a47925e25aadd5ef2f8684f33df01
wallecks <wallecksalencar9@gmail.com>
2023-04-18 20:26:19 -0300

    utilizando um array_keys

---
+++ array_keys.php
@@ -0,0 +1,17 @@
+<?php
+
+$inforCompany =[
+    'nome' => 'Escola',
+    'curso' => 'facimp',
+    'year' => 2023
+];
+var_dump($inforCompany);
+echo"\n";
+var_dump(array_keys($inforCompany)); // mostra as chaves que contem os valores
+
+
+//unset($inforCompany[/*especificar o valor da posiçao do array para ser excluida */]);
+
+    sort($inforCompany);// ordena os valoes de forma crescente
+var_dump($inforCompany); 
+?>
