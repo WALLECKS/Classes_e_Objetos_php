d045ad2001fa6809286bc949203543d7a480a965
wallecks <wallecksalencar9@gmail.com>
2023-04-18 21:01:33 -0300

    atualizado

--- retornando_erros.php
+++ retornando_erros.php
@@ -1,15 +1,22 @@
 <?php
 // abaixo criamos uma funcao Abrir que retorna informaçoes de existencia de arquivos
-function Abrir($classe_cliente = null){
-    if(!$classe_cliente)  { die ('falta o paramentro com o nome do arquivo'); } // verifica o nome do arquivo
+function Abrir($file = null) // informe o nome do arquivo a ser aberto
+{
+    if(!$file)  { die ('falta o paramentro com o nome do arquivo'); } // verifica o nome do arquivo
     
-    if(!file_exists($classe_cliente)){  die ('o arquivo nao existe'); } // verifica se existe o arquivo
+    if(!file_exists($file)){  die ('o arquivo nao existe'); } // verifica se existe o arquivo
 
-    if(!$retorno =@file_get_contents($classe_cliente)) { die ('impossivel ler o arquivo'); } // se nao retornar o arquivo entao sera impossivel ler o arquivo
+    if(!$retun =@file_get_contents($file)) { die ('impossivel ler o arquivo'); } // se nao retornar o arquivo entao sera impossivel ler o arquivo
 
     return true; // se existir o arquivo entao retorne verdadeiro
     
 }
 
+// especificar o caminho e o nome  do arquivo a ser aberto
+$arquivo = Abrir('/home/invisible_ghost/Documentos/estudos_PHP.classe.cliente.php');
+
+// mostra o arquivo
+
+echo ("$arquivo". "\n");
 
 ?>
