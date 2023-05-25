6d439e21faf78e589f0473231a527ac69b4abf22
wallecks <wallecksalencar9@gmail.com>
2023-05-24 21:24:15 -0300

    definiçao do metodo static

--- metodo_static.php
+++ metodo_static.php
@@ -1,6 +1,11 @@
 <?php
 class Login
 {
+    /*
+        Propriedades estaticas sao atributos de uma classe, sao dinamicas como as propriedades de um objeto, mas estao relacionadas à classe.
+        Como a classe é a estrutura comum a todos os objetos dela derivados, propriedades estaticas sao compartilhadas entre todos os objetos
+        de uma mesma classe.
+    */
     public static $usuario;
 
     public static function usuario_autenticado()
