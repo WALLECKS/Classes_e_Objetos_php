45c58c58b3e1b95ec05a15e0484cf5b6abb32948
wallecks <wallecksalencar9@gmail.com>
2023-07-14 15:00:12 -0300

    mostrando valores do banco de dados POSTGRESQL

--- CONEXAO_BD/mostrar_informaçoes_DB.php
+++ CONEXAO_BD/mostrar_informaçoes_DB.php
@@ -1,21 +1,21 @@
 <?php
 // ABRIR CONEXAO COM O BANCO DE DADOS POSTGRESQL
-$conectDB = pg_connect("host=localhost port = 5432 dbname = postgres2 = alex password = ");
+$conectdb = pg_connect("host=127.0.0.1 port=5432 dbname=postgres user=postgres password=Picole157");
 
 // DEFINE CONSULTA QUE SERA REALIZADA NO BANCO DE DADOS
-$query = 'SELECT codigo, nome FROM famosos';
+$query = 'SELECT nome, funçao FROM FUNCIONARIOS';
 
 // ENVIAR CONSUTA AO BANCO DE DADOS POSTGRESQL
-$result = pg_query($conectDB, $query);
+$result = pg_query($conectdb , $query);
 
 if ($result)
 {// PERCORRER RESULTADOS DA PESQUISA
     while( $row = pg_fetch_assoc($result))
     {
-        echo $row['codigo'].'-'. $row['nome'. "<br> \n"];
+        echo $row['nome'].'-'. $row['funçao'. "\n"];
     }
 }
 
 // FECHAR A CONEXAO
-pg_close($conectDB);
+pg_close($conectd);
 ?>
