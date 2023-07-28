7cc5ddfb9309e9d7d6db78b5ed2997fecaa6cd66
wallecks <wallecksalencar9@gmail.com>
2023-07-28 14:08:20 -0300

    manipulaçao de arquivos

--- public/2_manipulaçao_diretorios.php
+++ public/2_manipulaçao_diretorios.php
@@ -5,5 +5,12 @@ var_dump(mkdir('VIEWS'));   // cria uma pasta chamada VIEWS
 var_dump(rmdir('nome_do_diretorio')); // remove diretorios vazios
 var_dump(getcwd('')); // mostra o caminho em que o usuario se encontra, semelhante ao PWD
 var_dump(scandir('.')); // escaneia o caminho indicado entre '.' == caminho atual
-glob('../*.php'); // escaneia o caminho indicado entre  '', neste caso estamos escaneado todos os arquivos do diretorio atual que possuem extençoes .php 
+glob('../*.php'); // escaneia o caminho indicado entre '', neste caso estamos escaneado todos os arquivos do diretorio atual que possuem extençoes .php 
+var_dump(file_exists('images.php')); // verifica se existe o arquivo images.php
+touch('teste.txt'); //cria um arquivo 'nome.extençao'
+copy('nome_arquivo', 'novo_nome'); // copia um arquivo
+rename('images.php', 'images2.php'); // renomeia um arquivo
+var_dump(unlink('nome_arquivo')); // exclui um arquivo
+var_dump(file_put_contents('nome_arquivo','valor a ser passado')); // escreve um valor dentro de um arquivo
+var_dump(file_get_contents('nome_arquivo')); // retorna o conteudo de um arquivo
 ?>
